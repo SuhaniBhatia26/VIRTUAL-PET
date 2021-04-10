368b2dd7b40d6993ba957debe2fcfa2a833b19bc
SuhaniBhatia26 <74002572+SuhaniBhatia26@users.noreply.github.com>
2021-04-10 21:35:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
-var dog,sadDog,happyDog, database,firebase,data;
+var dog,sadDog,happyDog, database;
 var foodS,foodStock;
-var addFood,feed;
+var addFood;
 var foodObj;
-var FeedTime,lastFed;
 
-
+var feed , lastFed ;
 
 
 function preload(){
@@ -13,13 +12,13 @@ happyDog=loadImage("happy dog.png");
 }
 
 function setup() {
-  database=firebase.database();
+  //database=firebase.database();
   createCanvas(1000,400);
 
   foodObj = new Food();
 
-  foodStock=database.ref('Food');
-  foodStock.on("value",readStock);
+  //foodStock=database.ref('Food');
+  //foodStock.on("value",readStock);
   
   dog=createSprite(800,200,150,150);
   dog.addImage(sadDog);
@@ -40,12 +39,11 @@ function draw() {
   background(46,139,87);
   foodObj.display();
 
-  // to read fedtime value from the database 
+ // to read fedtime value from the database 
   //feedTime = database.ref('FeedTime');
   //feedTime.on("value",function(data){
   //lastFed = data.val();
-  
-
+   
   fill("red");
   textSize(25);
   if(lastFed>=12){
@@ -58,11 +56,8 @@ function draw() {
    //show time in AM format when lastFed is less than 12
   }
 
-
-  
-  
  
-  //write code to display text lastFed time here
+  
 
  
   drawSprites();
@@ -72,27 +67,30 @@ function draw() {
 function readStock(data){
   foodS=data.val();
   foodObj.updateFoodStock(foodS);
-
 }
 
+
 //function to update food stock and last fed time
-  function feedDog(){
-    dog.addImage(happyDog);
-  var food_stock_val = foodObj.getFoodStock();
-  if(food_stock_val <= 0){
-    foodObj.updateFoodStock(food_stock_val *0);
+function feedDog(){
+  dog.addImage(happyDog);
+var food_stock_val = foodObj.getFoodStock();
+if(food_stock_val <= 0){
+  foodObj.updateFoodStock(food_stock_val *0);
 
-  }
-  else{
-    foodObj.updateFoodStock(food_stock_val -1);
+}
+else{
+  foodObj.updateFoodStock(food_stock_val -1);
 
-  }
-  }
+}
+}
+
+
+
 
 //function to add food in stock
 function addFoods(){
   foodS++;
-  //database.ref('/').update({
+  database.ref('/').update({
     Food:foodS
-  //})
+  })
 }
